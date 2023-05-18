5f0294a30671e88681c2eebef140a4bd8f2f145f
Oscar McGoldrick <oscar.mcgoldrick99@gmail.com>
2023-05-19 10:08:03 +1200

    changing story data and adding public server info

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const { join } = require('node:path')
+const fs = require('fs').promises
 
 const routes = require('./routes')
 
@@ -10,5 +11,29 @@ server.set('view engine', 'hbs')
 server.set('views', join(__dirname, 'views'))
 server.use(express.urlencoded({ extended: true }))
 server.use('/', routes)
+
+// /public configuration
+const publicFolder = __dirname + '/public'
+server.use(express.static(publicFolder))
+
+server.get('/', (req, res) => {
+  res.render('home')
+})
+
+server.get('/game', (req, res) => {
+  fs.readFile(__dirname + '/data/story.JSON')
+    .then((storyString) => {
+      return JSON.parse(storyString)
+    })
+  res.render('game')
+})
+
+server.post('/game', (req, res) => {
+
+})
+
+server.get('result', (req, res) => {
+
+})
 
 module.exports = server
