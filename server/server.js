3f9910488260b918876ef0e721d78c93c4b0a62e
amanda <63027140+amanda-yvette-turnbull@users.noreply.github.com>
2023-05-19 11:33:31 +1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ const hbs = require('express-handlebars')
 const { join } = require('node:path')
 const fs = require('fs').promises
 
+const routes = require('./routes')
+
 const server = express()
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
-server.set('views', __dirname + '/views')
+server.set('views', join(__dirname, 'views'))
 server.use(express.urlencoded({ extended: true }))
+server.use('/', routes)
 
 // /public configuration
 const publicFolder = __dirname + '/public'
 server.use(express.static(publicFolder))
 
-
+module.exports = server
