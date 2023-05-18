a3bd774d1a6b40083666c6c274ffcd8cb091823f
amanda <63027140+amanda-yvette-turnbull@users.noreply.github.com>
2023-05-19 11:33:03 +1200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,3 +2,49 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs').promises
 
+
+router.get('/', (req, res) => {
+  res.render('home')
+})
+
+router.get('/home', (req, res) => {
+  fs.readFile('server/data/story.JSON')
+    .then((storyString) => {
+      return JSON.parse(storyString)
+    })
+    .then((storyObj) => {
+      const obj = storyObj.story[1]
+      res.render('home', obj)
+    })
+})
+
+router.get('/game', (req, res) => {
+  fs.readFile(__dirname + '/data/story.JSON')
+    .then((storyString) => {
+      return JSON.parse(storyString)
+    })
+    .then((storyObj) => {
+      res.render('game', storyObj)
+    })
+})
+
+router.post('/game', (req, res) => {
+  const buttonClicked = req.body.SOMETHING
+
+  fs.readFile(__dirname + '/data/story.JSON')
+    .then((storyString) => {
+      return JSON.parse(storyString)
+    })
+    .then((storyObj) => {
+      for (scenario in storyObj) {
+        
+      }
+    })
+  
+})
+
+router.get('result', (req, res) => {
+
+})
+
+module.exports = router
