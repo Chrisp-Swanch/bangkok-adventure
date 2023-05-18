f0e7fbabe12b312f5f08e906f7c07687ab4f78bd
Amanda <amandayvetteturnbull@gmail.com>
2023-05-19 11:04:57 +1200

    image

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,26 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index', { hi: 'Hello World!' })
+  const obj = {
+    id: '/call-mum',
+    image: '/phone.jpeg',
+    text: "You feel around for your phone. You breathe a sigh of relief when you find it in your back pocket. The screen is cracked. Also you're not sure if these are your pants or not.<br> <br>The phone flickers to life. You call your Mum. You hear an ominous beeping sound- the call didn't connect, as you're not on a Thai cell network. The phone dies.<br> <br>You begin to despair. What do you do now?",
+    options: [
+      {
+        buttonText: 'Try the hotel room phone',
+        toRoute: '/end',
+        message:
+          'You leave the bathroom and immediately trip up on your shoelaces.<br> <br>You died.',
+      },
+      {
+        buttonText: 'Give up and take a shower',
+        toRoute: '/shower',
+      },
+    ],
+  }
+  console.log(obj.image)
+
+  res.render('game', obj)
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,37 +3,32 @@ const hbs = require('express-handlebars')
 const { join } = require('node:path')
 const fs = require('fs').promises
 
-const routes = require('./routes')
-
 const server = express()
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
-server.set('views', join(__dirname, 'views'))
+server.set('views', __dirname + '/views')
 server.use(express.urlencoded({ extended: true }))
-server.use('/', routes)
 
 // /public configuration
 const publicFolder = __dirname + '/public'
 server.use(express.static(publicFolder))
+
+const routes = require('./routes')
+server.use('/', routes)
 
 server.get('/', (req, res) => {
   res.render('home')
 })
 
 server.get('/game', (req, res) => {
-  fs.readFile(__dirname + '/data/story.JSON')
-    .then((storyString) => {
-      return JSON.parse(storyString)
-    })
+  fs.readFile(__dirname + '/data/story.JSON').then((storyString) => {
+    return JSON.parse(storyString)
+  })
   res.render('game')
 })
 
-server.post('/game', (req, res) => {
+server.post('/game', (req, res) => {})
 
-})
-
-server.get('result', (req, res) => {
-
-})
+server.get('result', (req, res) => {})
 
 module.exports = server
