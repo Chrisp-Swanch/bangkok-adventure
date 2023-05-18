a565c65b2fbacc54155981b4911c9e79b4319bb1
Amanda <amandayvetteturnbull@gmail.com>
2023-05-19 11:16:19 +1200

    add kayne stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
   const obj = {
     id: '/call-mum',
-    image: '/phone.jpeg',
+    image: '/images/phone.jpeg',
     text: "You feel around for your phone. You breathe a sigh of relief when you find it in your back pocket. The screen is cracked. Also you're not sure if these are your pants or not.<br> <br>The phone flickers to life. You call your Mum. You hear an ominous beeping sound- the call didn't connect, as you're not on a Thai cell network. The phone dies.<br> <br>You begin to despair. What do you do now?",
     options: [
       {
